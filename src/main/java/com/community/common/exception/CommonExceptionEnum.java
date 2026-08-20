326d7ba9a273d88cf0761c7460ed8e123a23875c
soomin0209 <soomin02094@gmail.com>
2026-08-20 13:49:04 +0900

    feat: Redis 관련 커스텀 예외 추가

--- src/main/java/com/community/common/exception/CommonExceptionEnum.java
+++ src/main/java/com/community/common/exception/CommonExceptionEnum.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum CommonExceptionEnum implements ErrorCode {
     MISSING_IDEMPOTENCY_KEY(HttpStatus.BAD_REQUEST, "Idempotency-Key 헤더가 필요합니다"),
-    DUPLICATE_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "동일한 요청이 처리중입니다");
+    DUPLICATE_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "동일한 요청이 처리중입니다"),
+    REDIS_CONNECTION_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "일시적인 서버 오류로 요청을 처리할 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
